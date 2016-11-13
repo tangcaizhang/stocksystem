519ef3fc074ec9791e161a3ba66ddb35154337f5
ZACK <574437725@qq.com>
2016-11-14 02:09:22 +0800

    新增对StockSystemException的支持

--- app/src/main/java/zzh/project/stocksystem/helper/MsgHelper.java
+++ app/src/main/java/zzh/project/stocksystem/helper/MsgHelper.java
@@ -3,12 +3,15 @@ package zzh.project.stocksystem.helper;
 
 import com.android.volley.NetworkError;
 import com.android.volley.TimeoutError;
-import com.android.volley.VolleyError;
+
+import zzh.project.stocksystem.model.exception.StockSystemException;
 
 public class MsgHelper {
     // 从异常类转换成错误提示
-    public static String getErrorMsg(VolleyError error) {
-        if (error instanceof TimeoutError || error.getCause() instanceof TimeoutError) {
+    public static String getErrorMsg(Throwable error) {
+        if (error instanceof StockSystemException) {
+            return error.getMessage();
+        } else if (error instanceof TimeoutError || error.getCause() instanceof TimeoutError) {
             return "连接超时，请检测网络";
         } else if (error instanceof NetworkError || error.getCause() instanceof NetworkError) {
             return "请检测网络链接";
